052e2cb5277d50c4045c15881942f7a2cd6daec1
kgray3 <60368187+kgray3@users.noreply.github.com>
2023-04-27 11:11:40 -0400

    colors are now configurable

--- GraphPlotter.py
+++ GraphPlotter.py
@@ -7,15 +7,15 @@ from DataParser import DataParser
 # Add dual/triple Y-axis for conductivity and temperature
 class GraphPlotter:
     # Method to plot graphs for each file in one subplot
-    def plot_graph(files, y_axes, scales):
+    def plot_graph(files, y_axes, scales, colors):
         # Creates the subplot based on the # of files
         figure, axis = plt.subplots(len(files), constrained_layout = True)
         counter = 0
         
         #figure.subplots_adjust(right=0.75)
-        y_axes_colors = [None] * len(y_axes)
-        for z in range(len(y_axes)):
-            y_axes_colors[z] = np.random.rand(3)
+        y_axes_colors = colors
+        #for z in range(len(y_axes)):
+            #y_axes_colors[z] = np.random.rand(3)
         if len(files) == 1:
             # Convert date/time to timestamp for x-axis
             file = files[0]
@@ -36,13 +36,16 @@ class GraphPlotter:
                     axis.set_xlabel('Timestamp')
                     if axis_counter == 0 and file.setdefault(label):
                         y_axis = [eval (m) for m in file[label]]
-                        axis.plot(x_axis,y_axis)
+                        
                         if scales[label_counter] != 0:
                              axis.yticks(np.arange(min(y_axis), max(y_axis), scales[label_counter]))
                         axis.set_ylabel(label)
-                        axis.yaxis.label.set_color('b')
-                        axis.tick_params(axis='y', colors='b')
+                        print(y_axes_colors[label_counter])
+                        axis.yaxis.label.set_color(y_axes_colors[label_counter])
+                        axis.tick_params(axis='y', colors=y_axes_colors[label_counter])
+                        axis.plot(x_axis,y_axis,color=y_axes_colors[label_counter])
                         axis_counter = axis_counter + 1
+                        label_counter = label_counter + 1
                     else: 
                         if file.setdefault(label):
                             y_axis = [eval (m) for m in file[label]]
@@ -83,10 +86,10 @@ class GraphPlotter:
                         y_axis = [eval (m) for m in file[label]]
                         if scales[label_counter] != 0:
                              axis[counter].yaxis.set_ticks(np.arange(min(y_axis), max(y_axis), scales[label_counter]))
-                        axis[counter].plot(x_axis,y_axis)
+                        axis[counter].plot(x_axis,y_axis,y_axes_colors[label_counter])
                         axis[counter].set_ylabel(label)
-                        axis[counter].yaxis.label.set_color('b')
-                        axis[counter].tick_params(axis='y', colors='b')
+                        axis[counter].yaxis.label.set_color(y_axes_colors[label_counter])
+                        axis[counter].tick_params(axis='y', colors=y_axes_colors[label_counter])
                         axis_counter = axis_counter + 1
                         label_counter = label_counter + 1
                     else:
--- main.py
+++ main.py
@@ -7,20 +7,22 @@ from GraphPlotter import GraphPlotter
 #Log contains all of the data
 def main():
     
-    
+    #NOTE: CSV FILES MUST HAVE ONE LINE OF EMPTY SPACE AT THE TOP
+
     # Open files here - add variables for each file you want to open
-    f1_data = DataParser.parse_csv('F1\'19 Bostwick.csv')
+    #f1_data = DataParser.parse_csv('20 Spring Lowery In.csv')
     f2_data = DataParser.parse_csv('F1\'19 Lowery In.csv')
-    #f3_data = DataParser.parse_csv('F1\'19 Lowery Out.csv')
+    f3_data = DataParser.parse_csv('F1\'19 Lowery Out.csv')
     #f4_data = DataParser.parse_csv('F1\'19 Newton E.csv')
     #f5_data = DataParser.parse_csv('F1\'19 Newton W.csv')
-    f6_data = DataParser.parse_csv('June-Aug 2019 Weather Data.csv')
+    #f6_data = DataParser.parse_csv('Spring 2020 Weather Data.csv')
     #f7_data = DataParser.parse_csv('F1\'19 Phillip Out.csv')
     #f8_data = DataParser.parse_csv('F1\'19 Bostwick.csv')
 
+    
 
     # Add files to array
-    fileArr = [f1_data,f2_data,f6_data]
+    fileArr = [f2_data, f3_data]
 
     # use min/max to plot graph
     minimum, maximum = DataParser.parseDates(fileArr)
@@ -36,23 +38,17 @@ def main():
 
     # axes to be included for the data
     # level = blue, temperature = red  
-    y_axes = ['TEMPERATURE', 'CONDUCTIVITY',"Rain, mm (LGR S/N: 20624173, SEN S/N: 20627658)"] 
+    y_axes = ['TEMPERATURE', 'CONDUCTIVITY'] 
     
     # Y AXIS SCALES GET SET IN THIS ARRAY -- LINE UP WHAT SCALE YOU WANT
     # WITH THE CORRESPONDING LABEL IN y_axes -- 0 = USE DEFAULT SCALE
-    scales = [0,0,0]
-    GraphPlotter.plot_graph(graph_files,y_axes, scales)
+    scales = [0,0]
+    # Y AXIS COLORS GET SET IN THIS ARRA -- LINE UP WHAT COLOR YOU WANT
+    # WITH THE CORRESPONDING LABEL IN y_axes
+    colors = ['r','g']
+    GraphPlotter.plot_graph(graph_files,y_axes, scales, colors)
 
 
 
 main()
 
-# look into graphing library to display
-# multiple graphs side by side 
-# line up by shared timeframe
-# 8-9 maximum files of site data
-
-# 0.5) parse for date/time channels or separate date & time header 
-# 1) trim files by time frame = first step
-# 2) find graphing side-by-side stuff
-# maybe spit out into html or pdf?
